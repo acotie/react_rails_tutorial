f1f38f4092bed15d716e1e1bbcadc31768ba3d58
Akiko Yokoyama <acotie.acotie@gmail.com>
2016-06-21 23:09:51 +0900

    create controller & load jsx. GET /comments/index, comment_box.js.jsx

---
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -0,0 +1,30 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
