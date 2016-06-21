9881a2782b7e8524cc5a84a8485d157c9fec8f25
Akiko Yokoyama <acotie.acotie@gmail.com>
2016-06-22 02:34:33 +0900

    add event, change state: handleSubmit in onSubmit

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -20,12 +20,27 @@ var CommentBox = React.createClass({
     this.loadCommentsFromServer();
     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
+  handleCommentSubmit: function(comment) {
+    this.setState({data: this.state.data.concat([comment])});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data){
+        //this.setState({data: this.state.data.concat([data])});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.state.data}/>
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />  // send callback
       </div>
     );
   }
@@ -50,11 +65,25 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = ReactDOM.findDOMNode(this.refs.author).value.trim();
+    var text   = ReactDOM.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author){
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});  // run callback
+    ReactDOM.findDOMNode(this.refs.author).value = '';
+    ReactDOM.findDOMNode(this.refs.text).value = '';
+    return;
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
--- app/assets/javascripts/components/sample.js.jsx.coffee
+++
@@ -1,7 +0,0 @@
-$ ->
-  CommentBox = React.createClass
-    render: ->
-      `<div className="CommentBox">Hello, world! I am a CommentBox.</div>`
-
-  React.render `<CommentBox />`, document.getElementById('content')
-
